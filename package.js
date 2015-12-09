e62c826546c44d71487df8826d825ee38a2d72d9
Stephen Scott <me@suchipi.com>
2015-12-09 00:48:01 -0700

    bump version to 1.1.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'suchipi:irregular',
-  version: '1.1.2',
+  version: '1.1.3',
   summary: 'Abstracted, human-readable regular expressions',
   git: 'http://github.com/suchipi/irregular-js',
   documentation: 'README.md'
